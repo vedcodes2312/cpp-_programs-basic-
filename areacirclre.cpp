6144a8333c1d75e5bf66f9d1f742979423b1c9a8
Ved Vyas <vedvyascodes1608@gmail.com>
2024-10-22 23:33:43 +0530

    Add files via upload

---
+++ areacirclre.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>//for math functions
+using namespace std;
+int main()
+{
+    float radius;
+    cout <<  "Enter the radius of the circle: ";
+    cin  >> radius;
+    float area = M_PI *  pow(radius, 2);
+    float circumference = 2 * M_PI * radius;
+    cout <<"the area of circle with radius "<<radius<<" is "<<area<<endl;
+     cout <<"the circumference of circle with radius "<<radius<<" is "<<circumference;
+
+
+
+}
